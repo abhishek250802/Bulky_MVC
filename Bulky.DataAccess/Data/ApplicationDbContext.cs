bd97854c81231edb874c7e018688ccc77f9a69d7
abhishek250802 <abhishek.babusaheb508@gmail.com>
2023-07-18 19:12:30 +0530

    Add Foreign Key in Ef Core

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,21 +1,103 @@
-﻿using Bulky.Models;
+﻿﻿using BulkyBook.Models;
 using Microsoft.EntityFrameworkCore;
-
-namespace Bulky.DataAcess.Data
-
+namespace BulkyBook.DataAcess.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {
         }
+
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products{ get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Fortune of Time",
+                    Author = "Billy Spark",
+                    Description = "The Wheel of Fortune (1984) is a novel by Susan Howatch and recounts the trials and tribulations of a fictitious British family, the Godwins, who appear to be part of the minor aristocracy.",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                    CategoryId = 1
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Dark Skies",
+                    Author = "Nancy Hoover",
+                    Description = "Lydia is a scholar, but books are her downfall when she meddles in the plots of the most powerful man in the Celendor Empire. Her life in danger, she flees west to the far side of the Endless Seas and finds herself entangled in a foreign war where her burgeoning powers are sought by both sides. ",
+                    ISBN = "CAW777777701",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryId = 1
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Vanish in the Sunset",
+                    Author = "Julian Button",
+                    Description = "A blessed event becomes a nightmare for pregnant homicide detective Jane Rizzoli when she finds herself on the wrong side of a hostage crisis in this timely and relentless thriller. A nameless, beautiful woman appears to be just another corpse in the morgue. ",
+                    ISBN = "RITO5555501",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 40,
+                    Price100 = 35,
+                    CategoryId = 1
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Cotton Candy",
+                    Author = "Abby Muscles",
+                    Description = "'Cotton Candy' does not gloss over the brutal reality of child abuse; rather, L. M. Kimblin has crafted a hard hitting, raw account of the irreparable damage that child abuse has, and the lengths these poor young souls must to go in order to escape the constraints of monstrous oppressors and abusers. ",
+                    ISBN = "WS3333333301",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price50 = 60,
+                    Price100 = 55,
+                    CategoryId = 2
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Rock in the Ocean",
+                    Author = "Ron Parker",
+                    Description = "One day, a person tells the little rock that it has a great purpose waiting for it across the ocean and so the journey begins. Overcoming great obstacles and self doubt, this little rock will soon discover that our journey prepares us for our destination. ",
+                    ISBN = "SOTJ1111111101",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryId = 2
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Leaves and Wonders",
+                    Author = "Laura Phantom",
+                    Description = "It's free for life members. It's an employee of the medical profession, the life of Euismod wants sollicitud and. It's a very easy place to go. ",
+                    ISBN = "FOT000000001",
+                    ListPrice = 25,
+                    Price = 23,
+                    Price50 = 22,
+                    Price100 = 20,
+                    CategoryId = 3
+                }
                 );
         }
     }
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,4 +1,6 @@
-using Bulky.DataAcess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
 //var builder = WebApplication.CreateBuilder(args);   
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +12,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -26,5 +28,5 @@ app.UseRouting();
 app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 app.Run();
